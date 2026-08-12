801d8a620b049e3f89a2b67e8e25f518f2b6681a
felekemiryam1-ops <felekemiryam1@gmail.com>
2026-08-11 23:23:05 -0400

    multiple EC2 instances in config.json

--- aws_cost.py
+++ aws_cost.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 from cost_tools import calculate_total_cost, calculate_daily_estimate
 
@@ -12,42 +13,105 @@ prices = {
 
 def main():
 
-    # Check that the user provided 3 arguments
-    if len(sys.argv) != 4:
-        print("Usage: python aws_cost.py <instance> <hours> <servers>")
+    #Command-line arguments
+   
+    if len(sys.argv) == 4:
+
+        instance = sys.argv[1]
+
+        try:
+            hours = int(sys.argv[2])
+            servers = int(sys.argv[3])
+        except ValueError:
+            print("Hours and servers must be numbers.")
+            sys.exit()
+
+        try:
+            hourly_price = prices[instance]
+        except KeyError:
+            print("Unknown instance type.")
+            sys.exit()
+
+        total_cost = calculate_total_cost(
+            hourly_price,
+            hours,
+            servers
+        )
+
+        daily_estimate = calculate_daily_estimate(total_cost)
+
+        print("\nAWS COST ESTIMATE")
+        print("-----------------")
+        print(f"Instance: {instance}")
+        print(f"Hourly price: ${hourly_price:.4f}")
+        print(f"Hours: {hours}")
+        print(f"Servers: {servers}")
+        print(f"Monthly estimate: ${total_cost:.2f}")
+        print(f"Average daily estimate: ${daily_estimate:.2f}")
+
+
+    
+    #JSON configuration
+    
+    elif len(sys.argv) == 1:
+
+        try:
+            with open("config.json") as file:
+                config = json.load(file)
+        except FileNotFoundError:
+            print("config.json not found.")
+            sys.exit()
+
+        total_infrastructure_cost = 0
+
+        print("\nAWS INFRASTRUCTURE COST ESTIMATE")
+        print("--------------------------------")
+
+        for server in config["servers"]:
+
+            instance = server["instance"]
+            hours = server["hours"]
+            count = server["count"]
+
+            try:
+                hourly_price = prices[instance]
+            except KeyError:
+                print(f"Unknown instance type: {instance}")
+                continue
+
+            cost = calculate_total_cost(
+                hourly_price,
+                hours,
+                count
+            )
+
+            total_infrastructure_cost += cost
+
+            print(f"\nInstance: {instance}")
+            print(f"Hourly price: ${hourly_price:.4f}")
+            print(f"Hours: {hours}")
+            print(f"Servers: {count}")
+            print(f"Cost: ${cost:.2f}")
+
+        daily_estimate = calculate_daily_estimate(
+            total_infrastructure_cost
+        )
+
+        print("\n-------------------------------")
+        print(f"Total monthly cost: ${total_infrastructure_cost:.2f}")
+        print(f"Average daily cost: ${daily_estimate:.2f}")
+
+
+    
+
+    else:
+
+        print(
+            "Usage: python aws_cost.py "
+            "<instance> <hours> <servers>"
+        )
+
         sys.exit()
-
-    instance = sys.argv[1]
-
-    try:
-        hours = int(sys.argv[2])
-        servers = int(sys.argv[3])
-    except ValueError:
-        print("Hours and servers must be numbers.")
-        sys.exit()
-
-    try:
-        hourly_price = prices[instance]
-    except KeyError:
-        print("Unknown instance type.")
-        sys.exit()
-
-    total_cost = calculate_total_cost(
-        hourly_price,
-        hours,
-        servers
-    )
-
-    daily_estimate = calculate_daily_estimate(total_cost)
-
-    print("\nAWS COST ESTIMATE")
-    print("-----------------")
-    print(f"Instance: {instance}")
-    print(f"Hourly price: ${hourly_price:.4f}")
-    print(f"Hours: {hours}")
-    print(f"Servers: {servers}")
-    print(f"Monthly estimate: ${total_cost:.2f}")
-    print(f"Average daily estimate: ${daily_estimate:.2f}")
 
 
 main()
